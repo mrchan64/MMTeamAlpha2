7d8783bd049eee87acea415a0bd4103917ceb90b
Matt Chan <wildcatrobotics2011@gmail.com>
2019-02-10 13:13:29 -0800

    First Commit

---
+++ driver/example.hpp
@@ -0,0 +1,31 @@
+/***********************************************************************
+ *                                                                     *
+ *   This FILE sets up the LEDs and Voltmeter used in the Micromouse   *
+ *                                                                     *
+ *   Author: MATTHEW CHAN                                              *
+ *   Date: 2/10/2019                                                   *
+ *                                                                     *
+ ***********************************************************************/
+
+#include "mbed.h"
+
+// MAKE SURE TO CHANGE THIS WHEN YOU CREATE A NEW FILE
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+// CONSTANTS -------------------------------------------------
+const float EXAMPLE_CONSTANT                = 8.4f;
+const float EXAMPLE_CONSTANT_2              = 7.0f;
+
+// CLASSES ---------------------------------------------------
+class ExampleClass{
+public:
+	ExampleStruct(int param1);
+	void exampleMethod1();
+	void exampleMethod2(int param1, int param2);
+	DigitalOut exampleD1;
+};
+
+void exampleExternalMethod1(float param1);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+/***************************************************
+ *                                                 *
+ *   This FILE runs the main loop for Micromouse   *
+ *                                                 *
+ ***************************************************/
+
+// AUTHOR: Matthew Chan
+
+#include "mbed.h"
+
+int main() {
+  
+}
