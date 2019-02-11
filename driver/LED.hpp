daabf77e861dc8aa9061c9678e85fdaa4833bc1d
C734R <duypham12241999@gmail.com>
2019-02-10 17:39:17 -0800

    added function documentation

--- driver/LED.hpp
+++ driver/LED.hpp
@@ -13,15 +13,30 @@
 #ifndef LED_H
 #define LED_H
 
+//LED pins
 DigitalOut LED1(LED_1_PIN);
 DigitalOut LED2(LED_2_PIN);
 DigitalOut LED3(LED_3_PIN);
 DigitalOut LED4(LED_4_PIN);
 DigitalOut LED5(LED_5_PIN);
 
-void setLED(int id, boolean ledState);
+/** Sets specified LED on (ledValue = 1) or off (ledValue = 0)
+ *
+ */
+void setLED(int id, int ledValue);
+
+/** Sets all LEDs to display the binary number. For example, 00000 turns off
+ *  all LEDs and 11111 turns on all LEDs.
+ */
 void binaryLED(int num);
+
+/** Sets all LEDs to display a decimal number in binary format. For example,
+ *  3 turns off all LEDs except the last two (00011).
+ */
 void counterLED(int num);
+
+/** turns off all LEDs
+ */
 void allOffLED();
 
 #endif
