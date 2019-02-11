286c2bf63ccb9f3f5034d8a830e694054f7fcf63
C734R <duypham12241999@gmail.com>
2019-02-10 17:27:29 -0800

    Added binaryLED() body

--- driver/LED.cpp
+++ driver/LED.cpp
@@ -8,34 +8,47 @@
 *                                                                     *
 ***********************************************************************/
 
+#include <string>
+
 #include "mbed.h"
 #include "LED.h"
 
 void setLED(int id, boolean ledState) {
+  int ledValue = (ledState) ? 1 : 0;
   switch (id) {
     case 1:
-    LED1.write(ledState);
+    LED1 ledValue);
     break;
     case 2:
-    LED2.write(ledState);
+    LED2.write(ledValue);
     break;
     case 3:
-    LED3.write(ledState);
+    LED3.write(ledValue);
     break;
     case 4:
-    LED4.write(ledState);
+    LED4.write(ledValue);
     break;
     case 5:
-    LED5.write(ledState);
+    LED5.write(ledValue);
     break;
   }
 }
 
 void binaryLED(int num) {
+  string numString = std::to_string(num);
+  LED1.write(numString.at(0));
+  LED2.write(numString.at(1));
+  LED3.write(numString.at(2));
+  LED4.write(numString.at(3));
+  LED5.write(numString.at(4));
+}
 
+void counterLED(int num) {
+  
 }
 
 void allOffLED() {
+  LED1.write(0);
   LED2.write(0);
   LED3.write(0);
   LED4.write(0);
