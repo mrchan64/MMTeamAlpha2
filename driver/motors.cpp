aa6777f19f449ab9de14d9e5c0f73409711d1acd
C734R <duypham12241999@gmail.com>
2019-02-24 12:56:26 -0800

    created motors.cpp and motors.hpp

---
+++ driver/motors.cpp
@@ -0,0 +1,13 @@
+/***********************************************************************
+ *                                                                     *
+ *   This FILE sets up the motors in the Micromouse                      *
+ *                                                                     *
+ *   Author: DUY PHAM                                                  *
+ *   Date: 2/24/2019                                                   *
+ *   CPP File                                                          *
+ *                                                                     *
+ *                                                                     *
+ ***********************************************************************/
+
+ #include "mbed.h"
+ #include "motors.hpp"
