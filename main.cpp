620b729a0a5821f55891f10256153e4c40a751e9
Ajay Booter <abooter@ucsd.edu>
2019-04-14 16:51:00 -0700

    P in PID

--- main.cpp
+++ main.cpp
@@ -15,35 +15,53 @@ using namespace std;
 #include "driver/motors.hpp"
 #include "driver/encoders.hpp"
 #include "driver/LED.hpp"
-#include "algorithm/movement_states.hpp"
+// #include "algorithm/movement_states.hpp"
 
-extern MOVEMENT_STATE movementState;
+// extern MOVEMENT_STATE movementState;
 
 
 int main() {
-  // poll/
-
-  // check pause
-
-  // algorithm states
-
-  //runMovementStates();
-
+  comm.printf("UPLOADED\n\r");
   wait_ms(1000);
+
   initMotors();
   resetEncoders();
 
-  setMotorL(0.01f);
-  setMotorR(0.01f);
+  setMotorL(0.1f);
+  setMotorR(0.1f);
+
+  int targetDist = 400000;
   while(true) {
+    int leftDist = getPulseCountLeft();
+    int rightDist = getPulseCountRight();
 
-    //comm.printf("%d\n\r",getPulseCountLeft() - getPulseCountRight());
-    if( getPulseCountLeft() > 1024 ){
+    comm.printf("LEFT: %d\n\r", leftDist);
+    comm.printf("RIGHT: %d\n\r", rightDist);
+    comm.printf("DIFF: %d\n\r", rightDist - leftDist);
+    comm.printf("\%DIFF: %f\%\n\r", (rightDist - leftDist) / (float) targetDist * 100);
 
-      setMotorL(0.0f);
-      setMotorR(0.0f);
-      comm.printf("%d %d\n\r",getPulseCountLeft(), getPulseCountRight());
-    }
-    //wait_ms(1000);
+    float speedL = (targetDist - leftDist) * 0.000001f;
+    float speedR = (targetDist - rightDist) * 0.000001f;
+
+    if (speedL > 0.5f)
+      speedL = 0.5f;
+    else if (speedL < 0.0f)
+      speedL = 0.0f;
+
+    if (speedR > 0.5f)
+    speedR = 0.5f;
+    else if (speedR < 0.0f)
+      speedR = 0.0f;
+
+    comm.printf("LSPD: %f\n\r", speedL);
+    comm.printf("RSPD: %f\n\r", speedR);
+    comm.printf("\n\r");
+
+    setMotorL(speedL);
+    setMotorR(speedR);
+
+    resetEncoders();
+
+    wait_ms(100);
   }
 }
